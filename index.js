2080e00adc0b58ed61540817a0ccaeecc0a8765b
unknown <okwuosahpashal@gmail.com>
2021-09-26 23:10:34 +0100

    first commit

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const cors = require("cors");
 dotenv.config();
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
-var allowedOrigins = ['https://children-poppy.web.app'];
+var allowedOrigins = ['https://children-poppy.web.app', 'http://localhost:3000'];
 
 app.use(cors({
   origin: function (origin, callback) {
@@ -69,6 +69,29 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
+
+
+function saveImage(movie, imgEncoded) {
+  // CHECKING FOR IMAGE IS ALREADY ENCODED OR NOT
+  if (imgEncoded == null) return;
+
+  // ENCODING IMAGE BY JSON PARSE
+  // The JSON.parse() method parses a JSON string, constructing the JavaScript value or object described by the string
+  const img = JSON.parse(imgEncoded);
+  console.log( "JSON parse: "+ img);
+  
+  // CHECKING FOR JSON ENCODED IMAGE NOT NULL 
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types
+  // AND HAVE VALID IMAGE TYPES WITH IMAGE MIME TYPES
+  if (img != null && imageMimeTypes.includes(img.type)) {
+
+    // https://nodejs.org/api/buffer.html
+    // The Buffer class in Node.js is designed to handle raw binary data. 
+    // SETTING IMAGE AS BINARY DATA
+    movie.img = new Buffer.from(img.data, "base64");
+    movie.imgType = img.type;
+  }
+}
 
 app.listen("5000", () => {
   console.log("Backend is running.");
